f6dcd25fde18200de22561b0c5d175136b19f7ab
Engineer Localhost <engineerlocalhost@gmail.com>
2025-09-07 00:57:37 +0700

    update ubuntu

---
+++ ubuntu.py
@@ -0,0 +1,13 @@
+#Setelah install Ubuntu Desktop
+
+#change password root
+sudo passwd root #masukan password root
+
+#update & Upgrade
+apt update && apt upgrade -y
+
+#Installasi Apps pendukung
+1. apt install git #install git
+2. dpkg -i #install .*deb apps
+3. apt install golang-go #install golang
+    go version #check golang versi
